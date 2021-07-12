86d51933a8379b73a4f0f4458113ddfc2f2bb77c
seoyoung <seoyoung1113@naver.com>
2021-07-12 18:28:45 +0900

    Feat: 추가 로그인 화면xml, Feat: 추가 loginFragment  - close #3

--- app/src/main/java/com/hsu/mapapp/Fragments.kt
+++ app/src/main/java/com/hsu/mapapp/Fragments.kt
@@ -3,6 +3,7 @@ package com.hsu.mapapp
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.hsu.mapapp.databinding.ActivityLoginBinding
 import com.hsu.mapapp.databinding.ActivityMapBinding
 import com.hsu.mapapp.databinding.ActivityTestBinding
 
@@ -20,5 +21,11 @@ class TestFragment : Fragment(R.layout.activity_test) {
 class MapFragment : Fragment(R.layout.activity_map) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binding = ActivityMapBinding.bind(view)
+    }
+}
+
+class LoginFragment : Fragment(R.layout.activity_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = ActivityLoginBinding.bind(view)
     }
 }
