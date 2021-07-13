e125faf8d58580b99a4b97c5c488e092efa02c40
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2021-07-13 17:55:51 +0900

    설정 화면에서 버튼 클릭 시 다음 페이지로 전환 구현

---
+++ app/src/main/java/com/hsu/mapapp/AppinfoActivity.kt
@@ -0,0 +1,11 @@
+package com.hsu.mapapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AppinfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appinfo)
+    }
+}
--- app/src/main/java/com/hsu/mapapp/Fragments.kt
+++ app/src/main/java/com/hsu/mapapp/Fragments.kt
@@ -1,6 +1,7 @@
 package com.hsu.mapapp
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hsu.mapapp.databinding.ActivitySettingsBinding
 import com.hsu.mapapp.databinding.FragmentMapBinding
 import com.hsu.mapapp.databinding.FragmentMapListBinding
 
@@ -18,12 +20,48 @@ import com.hsu.mapapp.databinding.FragmentMapListBinding
 
 //class ShareFragment : Fragment(R.layout.레이아웃이름)
 
-//class SettingFragment : Fragment(R.layout.레이아웃이름)
 
 class SettingFragment : Fragment(R.layout.activity_settings) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private var _binding: ActivitySettingsBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivitySettingsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        SetBtnClickEvent()
+    }
+
+    private fun SetBtnClickEvent() {
+        binding.notice.setOnClickListener {
+            startActivity(Intent(context, NoticeActivity::class.java))
+        }
+
+        binding.noticeImage.setOnClickListener {
+            startActivity(Intent(context, NoticeActivity::class.java))
+        }
+
+        binding.appInfo.setOnClickListener {
+            startActivity(Intent(context, AppinfoActivity::class.java))
+        }
+
+        binding.appInfoImage.setOnClickListener {
+            startActivity(Intent(context, AppinfoActivity::class.java))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
 
 class TestFragment : Fragment(R.layout.activity_test) {
