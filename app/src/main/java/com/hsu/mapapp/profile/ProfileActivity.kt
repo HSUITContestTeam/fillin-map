5c921d4ba51573e02e0522f7ac795825ce0a460a
임유선 <1991167@hansung.ac.kr>
2021-07-30 23:11:10 +0900

    Fix: max heap bug

--- app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
+++ app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
@@ -3,12 +3,19 @@ package com.hsu.mapapp.profile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
 import com.hsu.mapapp.R
 
 import com.hsu.mapapp.databinding.ActivityProfileBinding
+import com.hsu.mapapp.login.CreateUserActivity
+import com.hsu.mapapp.login.LoginActivity
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var profileBinding : ActivityProfileBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +24,21 @@ class ProfileActivity : AppCompatActivity() {
 
         SetProfileModifyBtnClickEvent()
 
+        //firebase auth 객체
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        profileBinding.profileLogoutBtn.setOnClickListener {
+            signOut()
+        }
+
     }
     private fun SetProfileModifyBtnClickEvent() {
         profileBinding.profileModifyBtn.setOnClickListener {
             startActivity(Intent(this, ProfileModifyActivity::class.java))
             overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
         }
+    }
+
+    private fun signOut() { // 로그아웃
     }
 }
