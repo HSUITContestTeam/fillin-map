763d764371d779045718768065e426c329c44707
seoyoung <seoyoung1113@naver.com>
2021-07-30 17:43:41 +0900

    Feat: 추가 로그인 기능 #63

--- app/src/main/java/com/hsu/mapapp/login/CreateUserActivity.kt
+++ app/src/main/java/com/hsu/mapapp/login/CreateUserActivity.kt
@@ -2,6 +2,7 @@ package com.hsu.mapapp.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.auth.FirebaseAuth
@@ -10,9 +11,10 @@ import com.google.firebase.ktx.Firebase
 import com.hsu.mapapp.databinding.ActivityCreateUserBinding
 
 class CreateUserActivity : AppCompatActivity() {
-    private lateinit var createUseBinding:ActivityCreateUserBinding
+    private lateinit var createUseBinding: ActivityCreateUserBinding
     // Firebase Auth
     private lateinit var auth: FirebaseAuth
+    // android.util에서 제공하는 이메일 패턴
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,34 +24,58 @@ class CreateUserActivity : AppCompatActivity() {
         auth = Firebase.auth
         setJoinBtnEvent()
     }
+
     // 계정 생성
-    private fun createEmailUser() {
-        auth.createUserWithEmailAndPassword(createUseBinding.emailET.text.toString(),createUseBinding.passwordET.text.toString())
-            .addOnCompleteListener(this) {
-                task ->
+    private fun createEmailUser(email: EditText, password: EditText) {
+        auth.createUserWithEmailAndPassword(
+            email.text.toString(),
+            password.text.toString()
+        )
+            .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // 회원가입 성공
                     val currentUser = auth.currentUser
-                    Toast.makeText(this,"Authentication success", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
                     // 회원가입 액티비티 종료
-                    finishActivity(1)
-                }
-                else {
-                    // 회원가입 실패
-                    Toast.makeText(this,"Authentication failed", Toast.LENGTH_SHORT).show()
-
+                    finish()
+                } else {
+                    // 계정 중복
+                    Toast.makeText(this, "계정이 이미 있습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
-            .addOnFailureListener(OnFailureListener(){
+            .addOnFailureListener(OnFailureListener() {
                 it.printStackTrace()
             })
 
 
     }
+    // 이메일, 비밀번호 형식 체크
+    private fun checkForm(email: EditText, password: EditText): Boolean {
+        if (email.text.toString().isEmpty() || password.text.toString().isEmpty()) {
+            Toast.makeText(this, "이메일 혹은 비밀번호를 반드시 입력하세요", Toast.LENGTH_SHORT).show()
+            return false;
+        }
+        val emailPattern = android.util.Patterns.EMAIL_ADDRESS
+        if (!emailPattern.matcher(email.text.toString()).matches()) {
+            Toast.makeText(this, "이메일 형식을 확인하세요", Toast.LENGTH_SHORT).show()
+            return false;
+        }
+
+        if (password.text.toString().length < 6) {
+            Toast.makeText(this, "비밀번호를 6자 이상 입력하세요", Toast.LENGTH_SHORT).show()
+            return false;
+        }
+        return true;
+    }
     // 가입 버튼 이벤트
     private fun setJoinBtnEvent() {
         createUseBinding.joinBtn.setOnClickListener {
-            createEmailUser()
+            val email = createUseBinding.emailET
+            val password = createUseBinding.passwordET
+
+            if (checkForm(email,password)) {
+                createEmailUser(email,password)
+            }
         }
     }
 }
