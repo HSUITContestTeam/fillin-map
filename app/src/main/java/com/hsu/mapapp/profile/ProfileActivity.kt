f866b6b9dcfb545deb8452b20c045056d401cd04
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2021-08-10 23:49:12 +0900

    Merge branch 'CropImage' of C:\Users\SuBin\Documents\GitHub\map-app with conflicts.

--- app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
+++ app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
@@ -1,24 +1,37 @@
 package com.hsu.mapapp.profile
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import com.hsu.mapapp.R
 import com.hsu.mapapp.databinding.ActivityProfileBinding
 import com.hsu.mapapp.login.LoginActivity
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var profileBinding : ActivityProfileBinding
+    private lateinit var profileBinding: ActivityProfileBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private val TAG: String = "AppDebug"
+    private val GALLERY_REQUEST_CODE = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +40,32 @@ class ProfileActivity : AppCompatActivity() {
 
         setProfileModifyBtnClickEvent()
         setUpdatePasswordBtn()
-        profileBinding.logoutBtn.setOnClickListener {
-            signOut()
-        }
 
         profileBinding.logoutBtn.setOnClickListener {
-            revokeAccess()
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("정말로 로그아웃 하시겠습니까?")
+
+            builder.setPositiveButton("예") { dialog, which ->
+                signOut()
+            }
+            builder.setNegativeButton("아니오") { dialog, which ->
+                builder.setCancelable(true)
+            }
+            builder.show()
+        }
+
+        profileBinding.deleteAccountBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("정말로 탈퇴 하시겠습니까?")
+
+            builder.setPositiveButton("예") { dialog, which ->
+                revokeAccess()
+            }
+            builder.setNegativeButton("아니오") { dialog, which ->
+                builder.setCancelable(true)
+            }
+            builder.show()
+
         }
 
         //---------------------사용자 정보 load----------------------//
@@ -46,14 +79,26 @@ class ProfileActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         //----------------------------------------------------------//
 
+        // 프로필 표시
         setProfile()
 
+        // name 설정
+        profileBinding.buildNameIv.setOnClickListener {
+            updateName()
+        }
+
+        // 프로필 사진 설정
+        profileBinding.buildProfileIv.setOnClickListener {
+            //startActivity(Intent(this, CropImg::class.java))
+            pickFromGallery()
+        }
     }
 
+    //---------------------프로필 표시----------------------//
     private fun setProfile() {
         val user = Firebase.auth.currentUser
         if (user != null) {
@@ -72,14 +117,16 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
-    private fun setProfileModifyBtnClickEvent() { // 프로필 수정 버튼 이벤트
+    // 프로필 수정 버튼 이벤트
+    private fun setProfileModifyBtnClickEvent() {
         profileBinding.profileModifyBtn.setOnClickListener {
-            startActivity(Intent(this, ProfileModifyActivity::class.java))
+            startActivity(Intent(this, CropImg::class.java))
             overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right)
         }
     }
 
-    private fun signOut() { // 로그아웃
+    //---------------------로그아웃----------------------//
+    private fun signOut() {
         firebaseAuth.signOut() // Firebase sign out
         // Google sign out
         googleSignInClient.signOut().addOnCompleteListener(this) {
@@ -90,7 +137,8 @@ class ProfileActivity : AppCompatActivity() {
         startActivity(loginIntent)
     }
 
-    private fun revokeAccess() { //회원탈퇴
+    //---------------------회원 탈퇴----------------------//
+    private fun revokeAccess() {
         val user = Firebase.auth.currentUser!!
         signOut()
         user.delete()
@@ -103,11 +151,98 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     //---------------------비밀번호 재설정----------------------//
-    private fun setUpdatePasswordBtn() { // 비밀번호 재설정 버튼 이벤트
+    private fun setUpdatePasswordBtn() {
         profileBinding.passwordChangeBtn.setOnClickListener {
             startActivity(Intent(this, UpdatePasswordActivity::class.java))
             overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right)
         }
     }
 
+    //---------------------닉네임 재설정----------------------//
+    private fun updateName() {
+        val user = Firebase.auth.currentUser
+        var value = ""
+        val linearLayout = View.inflate(this, R.layout.dialog_name, null)
+        if (user != null) {
+            // 닉네임 입력 다이얼로그 설정
+            val builder = AlertDialog.Builder(this)
+                .setView(linearLayout)
+                .setPositiveButton("확인") { dialog, which ->
+                    val editText: EditText = linearLayout.findViewById(R.id.name_editText)
+                    value = editText.text.toString()
+                    // user auth 업데이트
+                    val profileUpdates = userProfileChangeRequest {
+                        displayName = value
+                        //photoUri = Uri.parse("https://example.com/jane-q-user/profile.jpg")
+                    }
+
+                    user!!.updateProfile(profileUpdates)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d("user name", "updated")
+                                // 바뀐 name text 설정
+                                profileBinding.profileNameTV.text = user.displayName
+                            }
+                        }
+                    // [END update_profile]
+                }
+                .setNegativeButton("취소") { dialog, which ->
+                    dialog.dismiss()
+                }
+            // 다이얼로그 실행
+            builder.show()
+
+        }
+    }
+
+    //------------------프로필 이미지 크롭----------------------//
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+
+            GALLERY_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.let { uri ->
+                        launchImageCrop(uri)
+                    }
+                }
+                else{
+                    Log.e(TAG, "Image selection error: Couldn't select that image from memory." )
+                }
+            }
+
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    setImage(result.uri)
+                }
+                else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Log.e(TAG, "Crop error: ${result.getError()}" )
+                }
+            }
+        }
+    }
+
+    private fun setImage(uri: Uri){
+        Glide.with(this)
+            .load(uri)
+            .into(profileBinding.profileImageIV)
+    }
+
+    private fun launchImageCrop(uri: Uri){
+        CropImage.activity(uri)
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .setCropShape(CropImageView.CropShape.RECTANGLE) // default is rectangle
+            .start(this)
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
 }
