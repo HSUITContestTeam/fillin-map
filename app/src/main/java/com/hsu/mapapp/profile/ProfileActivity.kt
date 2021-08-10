488054018be0f8d7551dc7070f18ba8ec571c422
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2021-08-11 00:56:14 +0900

    ProfileActivity.kt 이미지클릭리스너에 크롭이미지 기능 추가

--- app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
+++ app/src/main/java/com/hsu/mapapp/profile/ProfileActivity.kt
@@ -1,13 +1,17 @@
 package com.hsu.mapapp.profile
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -18,13 +22,16 @@ import com.google.firebase.ktx.Firebase
 import com.hsu.mapapp.R
 import com.hsu.mapapp.databinding.ActivityProfileBinding
 import com.hsu.mapapp.login.LoginActivity
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var profileBinding: ActivityProfileBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
-
+    private val TAG: String = "AppDebug"
+    private val GALLERY_REQUEST_CODE = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,7 +92,8 @@ class ProfileActivity : AppCompatActivity() {
 
         // 프로필 사진 설정
         profileBinding.buildProfileIv.setOnClickListener {
-            startActivity(Intent(this, CropImg::class.java))
+            //startActivity(Intent(this, CropImg::class.java))
+            pickFromGallery()
         }
     }
 
@@ -186,4 +194,54 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    //------------------프로필 이미지 크롭----------------------//
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+
+            GALLERY_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.let { uri ->
+                        launchImageCrop(uri)
+                    }
+                }
+                else{
+                    Log.e(TAG, "Image selection error: Couldn't select that image from memory." )
+                }
+            }
+
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    setImage(result.uri)
+                }
+                else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Log.e(TAG, "Crop error: ${result.getError()}" )
+                }
+            }
+        }
+    }
+
+    private fun setImage(uri: Uri){
+        Glide.with(this)
+            .load(uri)
+            .into(profileBinding.profileImageIV)
+    }
+
+    private fun launchImageCrop(uri: Uri){
+        CropImage.activity(uri)
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .setCropShape(CropImageView.CropShape.RECTANGLE) // default is rectangle
+            .start(this)
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
 }
