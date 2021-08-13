90be3d1c303610e2008967d01274fb86f7d185e3
yehang218 <yehang218@naver.com>
2021-08-14 03:11:40 +0900

    Feat : 추가 친구 검색 리사이클러뷰에 유저 목록 데이터 베이스 연결

--- app/src/main/java/com/hsu/mapapp/Share_Folder/FriendsSearchItemList.kt
+++ app/src/main/java/com/hsu/mapapp/Share_Folder/FriendsSearchItemList.kt
@@ -1,7 +1,7 @@
 package com.hsu.mapapp.Share_Folder
 
 data class FriendsSearchItemList (
-    val userId : String
+    val userId : String //연결하고자 하는 것과 이름이 일치해야함
 ) {
     constructor():this("")
 }
