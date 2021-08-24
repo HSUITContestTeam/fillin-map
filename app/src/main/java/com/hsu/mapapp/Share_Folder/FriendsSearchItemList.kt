752c5be989cabb3f7f7c1a98ed83c4fc3cfe06a4
seoyoung <seoyoung1113@naver.com>
2021-08-24 23:07:41 +0900

    Feat: 추가 friendsSearchItem에 uid

--- app/src/main/java/com/hsu/mapapp/Share_Folder/FriendsSearchItemList.kt
+++ app/src/main/java/com/hsu/mapapp/Share_Folder/FriendsSearchItemList.kt
@@ -1,7 +1,8 @@
 package com.hsu.mapapp.Share_Folder
 
 data class FriendsSearchItemList (
-    val userId : String //연결하고자 하는 것과 이름이 일치해야함
+    val userId : String, //연결하고자 하는 것과 이름이 일치해야함
+    val uid : String
 ) {
-    constructor():this("")
+    constructor():this("","")
 }
