96dce070f73a3458f3c07d9d8ad60cdb1508cca8
seoyoung <seoyoung1113@naver.com>
2021-08-26 18:10:55 +0900

    Feat: 추가 친구 요청 다이얼로그 #174

--- app/src/main/java/com/hsu/mapapp/Share_Folder/ShareFragment.kt
+++ app/src/main/java/com/hsu/mapapp/Share_Folder/ShareFragment.kt
@@ -1,13 +1,18 @@
 package com.hsu.mapapp.Share_Folder
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.hsu.mapapp.MainActivity
 import com.hsu.mapapp.R
 import com.hsu.mapapp.databinding.ActivityShareBinding
@@ -19,6 +24,9 @@ class ShareFragment : Fragment(R.layout.activity_share) {
     private val binding get() = _binding!!
     lateinit var text : String
     private lateinit var viewModel: ShareViewModel
+
+    private var firestore : FirebaseFirestore? = null
+    private val uid = Firebase.auth.currentUser ?.uid
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -37,14 +45,49 @@ class ShareFragment : Fragment(R.layout.activity_share) {
         viewModel = ViewModelProvider(this).get(ShareViewModel::class.java)
         // appbar - 뒤로 가기 버튼 없애기
         (activity as AppCompatActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(false)
-
+        firestore = FirebaseFirestore.getInstance()
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var myRef = firestore?.collection("users")?.document("$uid")
+        myRef!!.get()
+            .addOnSuccessListener { document->
+                val hashMap :Map<String,String>  = document.get("friendsList") as Map<String, String>
+                val keySet = hashMap.keys
+                for (key in keySet){
+                    Log.d("hashMapKey",key)
+                    if(hashMap[key].toString() == "requested"){
+                        val builder = AlertDialog.Builder(requireContext())
+                        builder
+                            .setTitle("친구 요청")
+                            .setMessage(key +"가 친구 요청을 보냈습니다. 수락하시겠습니까?")
+                            .setPositiveButton("예") {dialog, which ->
+                                myRef.update("friendsList", hashMapOf(
+                                    key to "friend"
+                                ))
+                                val friendRef = firestore
+                                    ?.collection("users")?.document(key)
+                                friendRef!!.get()
+                                    .addOnSuccessListener { document->
+                                        // uid에게 친구 요청을 받음
+                                        friendRef.update("friendsList", hashMapOf(
+                                            uid to "friend"
+                                        ))
 
+                                    }
+                            }
+                            .setNegativeButton("아니오") { dialog, which ->
+                                dialog.dismiss()
+                            }
+                            .show()
+
+                    }
+
+                }
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater){
