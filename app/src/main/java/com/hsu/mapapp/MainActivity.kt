9faab1c717dcca79a2dac748047b8b569c44a9cf
seoyoung <seoyoung1113@naver.com>
2021-09-05 13:57:34 +0900

    Feat: 추가 로그인시 토스트메시지 close #237

--- app/src/main/java/com/hsu/mapapp/MainActivity.kt
+++ app/src/main/java/com/hsu/mapapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hsu.mapapp
 
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -8,8 +10,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.hsu.mapapp.databinding.ActivityMainBinding
 
 
@@ -20,14 +23,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
 
     //private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var firebase: FirebaseFirestore
+    private val uid = Firebase.auth.currentUser?.uid
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(mainBinding.root)
-
         setNavigation()
+        firebase = FirebaseFirestore.getInstance()
+        WelcomeUserMessage()
+    }
+    private fun WelcomeUserMessage() {
 
+        val myRef = firebase.collection("users")?.document("$uid")
+        myRef.get()
+            .addOnSuccessListener {
+                Toast.makeText(this,it.get("name").toString()+"님 환영",Toast.LENGTH_LONG).show()
+            }
     }
 
     private fun setNavigation() {
