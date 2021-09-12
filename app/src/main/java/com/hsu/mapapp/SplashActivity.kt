372ad7b702f9c5b4dfc7e2a6937f54b9fa015a22
seoyoung <seoyoung1113@naver.com>
2021-09-12 20:09:17 +0900

    Design: 로그인 로딩화면 시간 늘림

--- app/src/main/java/com/hsu/mapapp/SplashActivity.kt
+++ app/src/main/java/com/hsu/mapapp/SplashActivity.kt
@@ -1,18 +1,18 @@
 package com.hsu.mapapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.hsu.mapapp.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME_OUT: Long = 1000//7300 // 1000 ->1sec
+    private val SPLASH_TIME_OUT: Long = 6100//7300 // 1000 ->1sec
     private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
